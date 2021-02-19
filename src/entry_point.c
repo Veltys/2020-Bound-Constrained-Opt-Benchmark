f4546ae96dc9ed9a75f9d7f1ca8a18a70a975d42
Veltys <veltys@veltys.es>
2021-02-19 18:39:10 +0100

    FIX: Vars and execution optimization

--- src/entry_point.c
+++ src/entry_point.c
@@ -9,31 +9,29 @@ void cec20_test_func(double*, double*, int, int, int);
 double *OShift, *M, *y, *z, *x_bound; // Las variables globales hacen llorar al niño Jesús
 int ini_flag = 0, n_flag, func_flag, *SS;
 
-void cec20_bench(unsigned int m, unsigned int n, double *x,
-		unsigned short int max_ejecuciones) {
-	unsigned short int i, j, k;
+void cec20_bench(const size_t m, const size_t n, double *x) {
+	unsigned short int i, j;
 	unsigned short int func_num = 1;
 	double *f;
 
 	f = (double*) malloc(sizeof(double) * m);
 
-	for(i = 0; i < max_ejecuciones; i++) {
-		if(x == NULL) {
-			printf("\nError: there is insufficient memory available!\n");
+	if(x == NULL) {
+		printf("\nError: there is insufficient memory available!\n");
 
-			exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
+	}
+
+	for(i = 0; i < D; i++) {
+		cec20_test_func(x, f, n, m, func_num);
+
+		for(j = 0; j < 2; j++) {
+			printf(" f%d(x[%d]) = %lf,", func_num, j + 1, f[j]);
 		}
-
-		for(j = 0; j < D; j++) {
-			cec20_test_func(x, f, n, m, func_num);
-
-			for(k = 0; k < 2; k++) {
-				printf(" f%d(x[%d]) = %lf,", func_num, k + 1, f[k]);
-			}
-			printf("\n");
-		}
+		printf("\n");
 
 	}
+
 	free(f);
 	free(y);
 	free(z);
