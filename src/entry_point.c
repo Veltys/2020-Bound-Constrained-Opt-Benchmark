9dd926bfd368ee292e08a18547cf464450d4b07c
Veltys <veltys@veltys.es>
2021-02-19 20:09:18 +0100

    FIX: Return

--- src/entry_point.c
+++ src/entry_point.c
@@ -9,8 +9,8 @@ void cec20_test_func(double*, double*, int, int, int);
 double *OShift, *M, *y, *z, *x_bound; // Las variables globales hacen llorar al niño Jesús
 int ini_flag = 0, n_flag, func_flag, *SS;
 
-void cec20_bench(const size_t m, const size_t n, double *x) {
-	unsigned short int i, j;
+double cec20_bench(const size_t m, const size_t n, double *x) {
+	unsigned short int i /*, j */;
 	unsigned short int func_num = 1;
 	double *f;
 
@@ -25,10 +25,12 @@ void cec20_bench(const size_t m, const size_t n, double *x) {
 	for(i = 0; i < D; i++) {
 		cec20_test_func(x, f, n, m, func_num);
 
-		for(j = 0; j < 2; j++) {
-			printf(" f%d(x[%d]) = %lf,", func_num, j + 1, f[j]);
-		}
-		printf("\n");
+		/*
+		 for(j = 0; j < 2; j++) {
+		 printf(" f%d(x[%d]) = %lf,", func_num, j + 1, f[j]);
+		 }
+		 printf("\n");
+		 */
 
 	}
 
@@ -38,5 +40,7 @@ void cec20_bench(const size_t m, const size_t n, double *x) {
 	// free(M);
 	// free(OShift);
 	// free(x_bound);
+
+	return f[2];
 }
 
